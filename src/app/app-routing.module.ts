04b07ab5f225efa0154439239c52e235efa43e0d
AimeeM <aimee.m.mascorro@gmail.com>
2019-11-12 14:53:49 -0500

    routing update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
