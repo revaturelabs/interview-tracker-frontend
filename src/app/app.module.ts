6cda3d8b5b8ffe705cb157df0774ee2a5fef7fa7
rizo85r <rizo85r@gmail.com>
2019-11-12 15:48:44 -0500

    cleaning html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-<<<<<<< HEAD
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingComponent } from './landing/landing.component';
 import { InsideComponent } from './inside/inside.component';
-=======
 import { HttpClientModule } from '@angular/common/http';
 
->>>>>>> 191e156e46031e6612ac392a26453dec84fd3015
+
 
 @NgModule({
   declarations: [
@@ -22,14 +20,11 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
     AppRoutingModule,
-    BrowserAnimationsModule
-=======
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
     AppRoutingModule
->>>>>>> 191e156e46031e6612ac392a26453dec84fd3015
   ],
   providers: [],
   bootstrap: [AppComponent]
