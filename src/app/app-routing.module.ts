591140f96358f4e815ce927b015eeea6bd36b353
jeffreydjamieson <jeffrey.david.jamieson@gmail.com>
2019-11-14 10:41:11 -0500

    changes from user feature applied

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { LandingComponent } from './landing/landing.component';
 import { HomeComponent } from './home/home.component';
+import {ProjectHubComponent} from './project-hub/project-hub.component';
 
 
 const routes: Routes = [
   {path: 'landing', component: LandingComponent},
   {path: '', pathMatch: 'full', redirectTo: 'login'},
   {path: 'login', component: LoginComponent },
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'hub', component: ProjectHubComponent}
   
 
 ];
