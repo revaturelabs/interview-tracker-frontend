14957e9cfd4703379f0233b5db1946da3ab56bda
rizo85r <rizo85r@gmail.com>
2019-11-14 09:19:17 -0500

    made assignments for tomrrow in hub component getting ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,13 @@ import { LoginComponent } from './components/login/login.component';
 import { LandingComponent } from './landing/landing.component';
 import { HomeComponent } from './home/home.component';
 
-
+// Ramses, plese make sure to route to the hub compnent upon login, and move your script from the index to the ts file. also, test our methos using jasmine
 const routes: Routes = [
-
   {path: 'landing', component: LandingComponent},
- {path: '', pathMatch: 'full', redirectTo: 'login'},
-
-
+  {path: '', pathMatch: 'full', redirectTo: 'login'},
   {path: 'login', component: LoginComponent },
-  {path: 'home', component: HomeComponent},  
+  {path: 'home', component: HomeComponent}
+  
 
 ];
 
