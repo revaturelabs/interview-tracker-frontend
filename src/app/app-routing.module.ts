fe277cfb4fb35d99408633f888190a3947fd933d
AimeeM <aimee.m.mascorro@gmail.com>
2019-11-14 16:25:16 -0500

    Made navbars, not styled yet tho

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { LandingComponent } from './landing/landing.component';
 import { HomeComponent } from './home/home.component';
+import { ProjectHubComponent } from './project-hub/project-hub.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 // Ramses, plese make sure to route to the hub compnent upon login, and move your script from the index to the ts file. also, test our methos using jasmine
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AppMaterialModule } from './app-material.module';
 
 import { HomeComponent } from './home/home.component';
 import { ProjectHubComponent } from './project-hub/project-hub.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 
@@ -26,7 +27,8 @@ import { ProjectHubComponent } from './project-hub/project-hub.component';
     LoginComponent,
     LandingComponent,
     InsideComponent,
-    ProjectHubComponent, 
+    ProjectHubComponent,
+    NavbarComponent, 
   ],
   
   imports: [
