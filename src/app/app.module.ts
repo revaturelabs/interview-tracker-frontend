d96cb9c06ef4407419dd08335374ca17b6c3287c
AimeeM <aimee.m.mascorro@gmail.com>
2019-11-15 10:26:44 -0500

    running without errors, still need to fix styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -46,12 +46,10 @@ import { ProfileFormComponent } from './components/profile-form/profile-form.com
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    
+    ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     AppMaterialModule,
-      
-
     BrowserModule,
     AppRoutingModule,
     FormsModule,
