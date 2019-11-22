e88f1970f93a7b0da57a3e37690358421719f6cd
jeffreydjamieson <jeffrey.david.jamieson@gmail.com>
2019-11-22 16:49:28 -0500

    coverage at 66.14%

--- src/app/components/logout/logout.component.spec.ts
+++ src/app/components/logout/logout.component.spec.ts
@@ -11,6 +11,9 @@ import { LandingComponent } from 'src/app/landing/landing.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule, MatFormFieldModule, MatCheckboxModule, MatAutocompleteModule, MatDatepickerModule } from '@angular/material';
 import { APP_BASE_HREF } from '@angular/common';
+import { ViewJobsComponent } from 'src/app/view-jobs/view-jobs.component';
+import { ViewProfilesComponent } from 'src/app/view-profiles/view-profiles.component';
+import { ViewInterviewsComponent } from 'src/app/view-interviews/view-interviews.component';
 
 describe('LogoutComponent', () => {
   let component: LogoutComponent;
@@ -18,7 +21,7 @@ describe('LogoutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LogoutComponent, LoginComponent, ProjectHubComponent, JobFormComponent, ProfileFormComponent, InterviewFormComponent, LandingComponent ],
+      declarations: [ LogoutComponent, LoginComponent, ViewJobsComponent, ViewProfilesComponent, ViewInterviewsComponent, ProjectHubComponent, JobFormComponent, ProfileFormComponent, InterviewFormComponent, LandingComponent ],
       imports: [HttpClientModule, AppRoutingModule, FormsModule, MatCardModule, MatFormFieldModule, MatCheckboxModule, MatAutocompleteModule, MatDatepickerModule, ReactiveFormsModule],
       providers: [{provide: APP_BASE_HREF, useValue: '/my/app'}]
     })
