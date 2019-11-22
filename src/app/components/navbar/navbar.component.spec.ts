310b94da5815ee38774efc56fafd6d959e174c8d
jeffreydjamieson <jeffrey.david.jamieson@gmail.com>
2019-11-22 10:54:42 -0500

    Testing update: current coverage 67.12%

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -1,6 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { MatToolbarModule, MatSidenavModule, MatListModule, MatCardModule, MatFormFieldModule, MatCheckboxModule, MatOptionModule, MatAutocompleteModule, MatDatepickerModule } from '@angular/material';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { LoginComponent } from '../login/login.component';
+import { ProjectHubComponent } from 'src/app/project-hub/project-hub.component';
+import { JobFormComponent } from '../job-form/job-form.component';
+import { InterviewFormComponent } from '../interview-form/interview-form.component';
+import { ProfileFormComponent } from '../profile-form/profile-form.component';
+import { LandingComponent } from 'src/app/landing/landing.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { APP_BASE_HREF } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('SidenavComponent', () => {
   let component: NavbarComponent;
@@ -8,7 +19,9 @@ describe('SidenavComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+      declarations: [ NavbarComponent, LoginComponent, ProjectHubComponent, JobFormComponent, InterviewFormComponent, ProfileFormComponent, LandingComponent ],
+      imports: [BrowserAnimationsModule,MatToolbarModule, AppRoutingModule, MatSidenavModule, MatFormFieldModule, MatListModule, MatCardModule, FormsModule, MatCheckboxModule, MatOptionModule, MatAutocompleteModule, MatDatepickerModule, ReactiveFormsModule],
+      providers: [{provide: APP_BASE_HREF, useValue: '/my/app'}]
     })
     .compileComponents();
   }));
