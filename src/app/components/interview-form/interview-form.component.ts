0b10927e7a54be21bd9a4d3299f8175a860a2704
tupyl2 <lyssamt@gmail.com>
2019-11-25 21:05:00 -0500

    adding more features

--- src/app/components/interview-form/interview-form.component.ts
+++ src/app/components/interview-form/interview-form.component.ts
@@ -9,6 +9,9 @@ import { startWith, map } from 'rxjs/operators';
 export interface User {
   firstName: any;
 }
+export interface Job {
+  title: any;
+}
 
 @Component({
   selector: 'app-interview-form',
@@ -17,32 +20,59 @@ export interface User {
 })
 export class InterviewFormComponent implements OnInit {
   myControl = new FormControl();
+  jobControl = new FormControl();
 
   options: User[] = [];
+  jobOptions: Job[] = [];
   profiles: any;
+  jobs: any;
   filteredOptions: Observable<User[]>;
+  filteredJobOptions: Observable<Job[]>;
   constructor( public nav: NavbarservService, private http: HttpClient) { }
 
   ngOnInit() {
     this.nav.show();
+
     this.http.get(environment.main_url + 'profiles/allProfiles').toPromise().then(r => {
       this.profiles = r;
       console.log(this.profiles);
       for (let p of this.profiles) {
         this.options.push({firstName: p.firstName});
+        console.log(this.options);
       }
-      console.log(this.options);
     });
+
+    this.http.get(environment.main_url + 'jobs/allJobs').toPromise().then(s => {
+      this.jobs = s;
+      console.log(this.jobs);
+      for (let j of this.jobs) {
+        this.jobOptions.push({title: j.title});
+      }
+      console.log(this.jobOptions);
+    });
+
     this.filteredOptions = this.myControl.valueChanges
       .pipe(
         startWith(''),
         map(value => typeof value === 'string' ? value : value.firstName),
         map(firstName => firstName ? this._filter(firstName) : this.options.slice())
       );
+
+    this.filteredJobOptions = this.jobControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => typeof value === 'string' ? value : value.title),
+        map(title => title ? this._jobfilter(title) : this.jobOptions.slice())
+      );
+
   }
 
   displayFn(user?: User): string | undefined {
     return user ? user.firstName : undefined;
+  }
+
+  displayJ(job?: Job): string | undefined {
+    return job ? job.title : undefined;
   }
 
   private _filter(firstName: string): User[] {
@@ -50,6 +80,15 @@ export class InterviewFormComponent implements OnInit {
 
     return this.options.filter(option => option.firstName.toLowerCase().indexOf(filterValue) === 0);
   }
+
+  private _jobfilter(title: string): Job[] {
+    const jobFilterValue = title.toLowerCase();
+
+    return this.jobOptions.filter(jobOption => jobOption.title.toLowerCase().indexOf(jobFilterValue) === 0);
   }
+
+   }
+
+
 
 
