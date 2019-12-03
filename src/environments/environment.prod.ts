f16ac0b114c265275b721cd127844c3dd730e98a
connellrobert <robert.lel.connell@gmail.com>
2019-12-03 12:44:00 -0500

    env update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  login_url : ' http://54.183.8.63:8765/user-service/',
-  main_url: ' http://54.183.8.63:8765/interview-service/'
+  login_url : ' http://54.183.231.146:8765/user-service/',
+  main_url: ' http://54.183.231.146:8765/interview-service/'
 };
