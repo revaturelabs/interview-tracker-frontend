994c4ea12acab6cb212e8c27366fdb9bda24dafc
jeffreydjamieson <jeffrey.david.jamieson@gmail.com>
2019-12-02 21:24:11 -0500

    coverage should be ~52%

--- src/app/project-hub/project-hub.component.spec.ts
+++ src/app/project-hub/project-hub.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProjectHubComponent } from './project-hub.component';
+import { NavbarservService } from '../services/navbarserv.service';
 
 describe('ProjectHubComponent', () => {
   let component: ProjectHubComponent;
@@ -23,7 +24,16 @@ describe('ProjectHubComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should hide the navbar', ()=>{
+    let tester=new ProjectHubComponent(new NavbarservService);
+    expect(tester.show).toBeFalsy();
+  });
 
+  // it('should show the navbar after toggle is called', ()=>{
+  //   let tester=new ProjectHubComponent(new NavbarservService);
+  //   tester.toggle();
+  //   expect(tester.show).toBeTruthy();
+  // });
 
 
 });
