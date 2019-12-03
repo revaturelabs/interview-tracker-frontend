9c5d01ef1f38a5f0dda85d0cd5cf83027d2214fb
tupyl2 <lyssamt@gmail.com>
2019-12-02 22:05:08 -0500

    Now hitting back only redirects to login, edited

--- src/app/project-hub/project-hub.component.ts
+++ src/app/project-hub/project-hub.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavbarservService } from '../services/navbarserv.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-project-hub',
@@ -10,7 +11,7 @@ export class ProjectHubComponent implements OnInit {
   public show:boolean = false;
   public buttonName:any = 'Show';
 
-  constructor( public nav: NavbarservService ) { }
+  constructor( public nav: NavbarservService, private activatedRoute: ActivatedRoute ) { }
 
   ngOnInit() {
     if (!localStorage.getItem('loa')) { 
@@ -20,7 +21,7 @@ export class ProjectHubComponent implements OnInit {
       localStorage.removeItem('loa') 
     }
     this.nav.show();
-  }
+   }
 
   toggle() {
     this.show = !this.show;
