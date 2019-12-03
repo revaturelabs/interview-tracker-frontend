597a7553d8005e90c7374500c1093abc23a1e733
connellrobert <robert.lel.connell@gmail.com>
2019-12-03 12:14:34 -0500

    fixed environment and compile issues with binding

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  login_url : ' http://54.183.8.63',
-  main_url: ' http://54.183.8.63'
+  login_url : ' http://54.183.8.63:8765/user-service/',
+  main_url: ' http://54.183.8.63:8765/interview-service/'
 };
