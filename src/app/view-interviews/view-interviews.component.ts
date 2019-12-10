ed40fc0241f9f3b2ac0642e73eeb028868ab9d5b
sekhemramin <sekhemramin@outlook.com>
2019-12-09 18:50:25 -0600

    had to change the way that inteview objects are sent along with a comment object

--- src/app/view-interviews/view-interviews.component.ts
+++ src/app/view-interviews/view-interviews.component.ts
@@ -4,6 +4,9 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { InterviewService } from '../services/Interview/interview.service';
 import { Interview } from '../models/interview';
+import { FormControl } from '@angular/forms';
+import { Comment } from 'src/app/models/comment';
+import { CommentService } from '../services/Comment/comment.service';
 
 @Component({
   selector: 'app-view-interviews',
@@ -12,7 +15,14 @@ import { Interview } from '../models/interview';
 })
 export class ViewInterviewsComponent implements OnInit {
 interviews: Interview[];
-  constructor(public nav: NavbarservService, private http: HttpClient, private interviewService: InterviewService) { }
+  constructor(public nav: NavbarservService, 
+    private http: HttpClient, 
+    private interviewService: InterviewService,
+    private commentService: CommentService) { }
+
+  comment: Comment;
+  updatedInterview: Interview;
+  foundInterviewId: number;
 
   ngOnInit() {
     this.nav.show();
@@ -20,9 +30,40 @@ interviews: Interview[];
   }
   getAllInterviews() {
     this.interviewService.getAllInterviews().subscribe(i => {
-      console.log(i);
       this.interviews = i;
+
     }, err => console.log(err))
   }
+  nameFC = new FormControl();
+  commentFC = new FormControl();
+  intId: number;
 
-}
+  sendComment(intId){
+      let timeStamp = new Date();
+      this.commentService.grabInterview(intId).subscribe(
+        data => {
+          console.log(data);
+            this.commentService.addComment(
+                    data["id"], 
+                    this.comment = new Comment(
+                      timeStamp,
+                      this.nameFC.value,
+                      this.commentFC.value,
+                      this.updatedInterview = data
+                    )
+                  ).subscribe(
+                        data => {
+                          console.log(data)
+                        },
+                        error => {
+                          console.log(error)
+                        }
+                  )
+        },
+        error => {
+          console.log(error)
+        }
+      )
+    }
+  }
+
