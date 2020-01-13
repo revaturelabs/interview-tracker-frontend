0b63ee3ec938610f498ff5c837243c0f9fb6a959
Daniel Vicenti <38819937+dvicenti96@users.noreply.github.com>
2020-01-13 17:00:05 -0500

    Added pagination function

--- src/app/pages/jobs/jobs.component.ts
+++ src/app/pages/jobs/jobs.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { JobServiceService } from 'src/app/Job-Service/job-service.service';
+import Job from 'src/app/models/Job';
 
 @Component({
   selector: 'app-jobs',
   templateUrl: './jobs.component.html',
   styleUrls: ['./jobs.component.scss']
 })
-export class JobsComponent implements OnInit {
-  constructor() {}
+export class JobsComponent implements OnInit 
+{
+  private jobs: Job[];
 
-  ngOnInit() {}
+  constructor(private jobServ: JobServiceService) {}
+
+  ngOnInit() {
+  }
 
   onSearchTermChanged(e) {
     console.log('inside of job.component, value is: ', e);
+  }
+
+  retrieveJobPage(page: number)
+  {
+    this.jobServ.getAllJobAtPage(page).subscribe(data => {
+      this.jobs.concat(data);
+    })
   }
 }
