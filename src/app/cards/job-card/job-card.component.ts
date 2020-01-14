cdc0f1e4858dd91d953047323d0c06e5eda6d459
Jared Chesnut <red.chesnut@yahoo.com>
2020-01-14 18:20:49 -0500

    Realized that a job-card already exists. Removed newly created one, added changes to old. -redc

--- src/app/cards/job-card/job-card.component.ts
+++ src/app/cards/job-card/job-card.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import Job from 'src/app/models/Job';
+import Card from 'src/app/cards/Card';
 
 @Component({
   selector: 'app-job-card',
   templateUrl: './job-card.component.html',
   styleUrls: ['./job-card.component.scss']
 })
-export class JobCardComponent implements OnInit {
+export class JobCardComponent extends Card implements OnInit {
 
-  constructor() { }
+  sampleTitle = 'Freelance Sloth Manager';
+  sampleDescription = 'To ensure that Slothful practices are maintained and followed by the organization.';
+
+
+  constructor() {
+    super();
+   }
 
   ngOnInit() {
+  }
+
+  getJob(job: Job): Job {
+    // Gets job from Job-Service, passes params.
+    return job;
   }
 
 }
