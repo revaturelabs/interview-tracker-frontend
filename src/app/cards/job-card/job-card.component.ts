cc56bd415f6d510cb3234627c5669362cf4ae4a8
Jared Chesnut <red.chesnut@yahoo.com>
2020-01-14 18:49:24 -0500

    Realized that a job-card already exists. Removed newly created one, added changes to old. -redc

--- src/app/cards/job-card/job-card.component.ts
+++ src/app/cards/job-card/job-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import Job from 'src/app/models/Job';
 import Card from 'src/app/cards/Card';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-job-card',
