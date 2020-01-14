abae6ef25128273e4473f36cb9339bd97b0aec6b
Daniel Vicenti <38819937+dvicenti96@users.noreply.github.com>
2020-01-14 10:22:05 -0500

    Finished Interview List
    This now replaces the multi-select component, so that it can output selected values for the interview page.

--- src/app/interviewer-list/interviewer-list.component.ts
+++ src/app/interviewer-list/interviewer-list.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { HttpService } from '../http.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-interviewer-list',
   templateUrl: './interviewer-list.component.html',
   styleUrls: ['./interviewer-list.component.scss']
 })
-export class InterviewerListComponent implements OnInit {
+export class InterviewerListComponent implements OnInit 
+{
+  selections = new FormControl();
+  @Input() selectionList: string[];
+  @Input() title: string;
 
-  constructor() { }
+  @Output() selectionChange = new EventEmitter();
+  
+  constructor(private http: HttpService) { }
 
-  ngOnInit() {
+  ngOnInit() 
+  {
+
+  }
+
+  selectionChanged()
+  {
+    this.selectionChange.emit(this.selections.value);
   }
 
 }
