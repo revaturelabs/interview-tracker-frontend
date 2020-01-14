02a3a57e7ad1321b15267ed897737afaea44b617
Ryan Kim <ryankeehoon@gmail.com>
2020-01-14 10:40:54 -0500

    Update profile.service.ts

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -10,13 +10,33 @@ export class ProfileService {
   constructor(private http: HttpClient) { }
 
   retrieveAllProfiles(){
-    const url = "http://localhost:8765/profiles/allProfiles";
+    const url = "http://localhost:8765/interview-service/profiles/allProfiles";
     return this.http.get<Profile[]>(url, {});
   }
 
   saveProfile(newProfile: Profile){
-    const url = "http://localhost:8765/profiles/saveProfile";
+    const url = "http://localhost:8765/interview-service/profiles/saveProfile";
     this.http.post<Profile>(url, newProfile);
   }
 
-}
+
+
+
+  // constructor(private http: HttpClient) { }
+
+  // createNewUser(form: any): Observable<number> {
+
+  //   const params = new HttpParams()
+  //   .set('username', form.value.username)
+  //   .set('userpassword', form.value.password)
+  //   .set('userfname', form.value.firstname)
+  //   .set('userlname', form.value.lastname)
+  //   .set('useremail', form.value.email)
+  //   .set('userbirthday', form.value.dob)
+  //   .set('usertitle', form.value.title)
+  //   .set('userbio', '')
+  //   .set('userprofilepic', 'https://campfire-project2.s3.us-east-2.amazonaws.com/profile_picture.svg');
+
+  //   return this.http.post<number>(this.url, params, {withCredentials: true});
+  }
+
