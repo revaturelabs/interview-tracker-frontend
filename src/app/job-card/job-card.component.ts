aa38dc872c3499c6e3758de2e6459f29ee8b580e
Jared Chesnut <red.chesnut@yahoo.com>
2020-01-14 17:24:57 -0500

    job-card Component added. Will look to work on structure during the evening. -redc

--- src/app/job-card/job-card.component.ts
+++ src/app/job-card/job-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Job from '../models/Job';
 
 @Component({
   selector: 'app-job-card',
@@ -10,6 +11,10 @@ export class JobCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getJob(job: Job) {
+    // Gets job from Job-Service, passes params.
   }
 
 }
