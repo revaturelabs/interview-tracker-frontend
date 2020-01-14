4199e70ea387d1f4b3ceb18d90b25f096ee344d7
Jared Chesnut <red.chesnut@yahoo.com>
2020-01-14 17:13:24 -0500

    job-card Component added. Will look to work on structure during the evening. -redc

---
+++ src/app/job-card/job-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-card',
+  templateUrl: './job-card.component.html',
+  styleUrls: ['./job-card.component.scss']
+})
+export class JobCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
