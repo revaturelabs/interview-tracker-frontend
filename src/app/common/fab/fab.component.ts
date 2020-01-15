c36e30033b686187737ce5b9927e1226bdcc57af
Miranda_Brawner <mirandawbrawner@gmail.com>
2020-01-14 20:39:17 -0500

    Added to fab component.
    Added code to the html and ts files of the fab component so that the state of the menu (whether it is open or closed) is stored. It also responds to button presses by creating tags for the requested components.

--- src/app/common/fab/fab.component.ts
+++ src/app/common/fab/fab.component.ts
@@ -6,15 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fab.component.scss']
 })
 export class FabComponent implements OnInit {
+
   activated = false;
+  returnedContent = '';
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  activate() {
+  activate(): void {
     this.activated = !this.activated;
   }
 
+  clickCreateJob(): void {
+    this.returnedContent = '<app-create-jobs />';
+  }
+
+  clickCreateProfile(): void {
+    this.returnedContent = '<app-profile-create />';
+  }
 }
