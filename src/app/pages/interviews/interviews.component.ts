1847586c848953437d253a01bed356023ffa3cc3
Jack McCormick <57637313+JackMcCormick@users.noreply.github.com>
2020-01-15 10:44:22 -0500

    Fixing Interview Card Info
    Now pulls from database call instead of dummy data. I left the dummy data call in commented

--- src/app/pages/interviews/interviews.component.ts
+++ src/app/pages/interviews/interviews.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../../http.service';
 import Interview from 'src/app/models/Interview';
+import { InterviewService } from '../../interview-service/interview.service';
 
 @Component({
   selector: 'app-interviews',
@@ -12,10 +13,12 @@ export class InterviewsComponent implements OnInit {
 
   interviews: Interview[] = [];
 
-  constructor(private httpService: HttpService) {}
+  constructor(private httpService: HttpService, private interviewService: InterviewService) {}
 
   ngOnInit() {
     this.interviewers = this.httpService.interviewers;
-    this.httpService.getJSON('assets/seed/interviews.json').subscribe(interviewList => this.interviews = interviewList);
+    this.interviewService.retrieveAllInterviews().subscribe(interviewList => this.interviews = interviewList);
+    // Dummy Data v
+    // this.httpService.getJSON('assets/seed/interviews.json').subscribe(interviewList => this.interviews = interviewList);
   }
 }
