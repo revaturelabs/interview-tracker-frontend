c6362c11afa91bc05ddb5f2c73fd326766deac3d
Jack McCormick <57637313+JackMcCormick@users.noreply.github.com>
2020-01-15 10:53:44 -0500

    Update interviews.component.ts

--- src/app/pages/interviews/interviews.component.ts
+++ src/app/pages/interviews/interviews.component.ts
@@ -9,14 +9,12 @@ import { InterviewService } from '../../interview-service/interview.service';
   styleUrls: ['./interviews.component.scss']
 })
 export class InterviewsComponent implements OnInit {
-  interviewers: string[];
 
   interviews: Interview[] = [];
 
-  constructor(private httpService: HttpService, private interviewService: InterviewService) {}
+  constructor(private interviewService: InterviewService) {}
 
   ngOnInit() {
-    this.interviewers = this.httpService.interviewers;
     this.interviewService.retrieveAllInterviews().subscribe(interviewList => this.interviews = interviewList);
     // Dummy Data v
     // this.httpService.getJSON('assets/seed/interviews.json').subscribe(interviewList => this.interviews = interviewList);
