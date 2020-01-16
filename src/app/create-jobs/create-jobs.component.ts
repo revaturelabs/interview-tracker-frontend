65d529629c5a8bcb151b43f3f73c20c49d02f0f0
wonder590 <shahen.r.melkonian@gmail.com>
2020-01-16 14:48:37 -0500

    got skills in

--- src/app/create-jobs/create-jobs.component.ts
+++ src/app/create-jobs/create-jobs.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { ProfileService } from '../profile.service';
 import { SkillService } from '../skill.service';
+import { FormControl } from '@angular/forms';
+import Skill from 'src/app/models/Skill';
 
 @Component({
   selector: 'app-create-jobs',
@@ -11,7 +13,25 @@ export class CreateJobsComponent implements OnInit {
 
   constructor(private profServ: ProfileService, private skillServ: SkillService) { }
 
-  ngOnInit() {
+  @Output() selections = new FormControl();
+  title = 'Skills';
+  options: Skill[];
+  allSkills: Skill[];
+
+  termChanged(event) {
+    this.options = this.allSkills.filter(el =>
+      el.title.includes(event.target.value)
+    );
   }
 
+  resetOptions() {
+    this.options = this.allSkills;
+  }
+
+  ngOnInit() {
+    this.skillServ
+      .retrieveAllSkills()
+      .subscribe(list => {this.allSkills = list; console.log(list)});
+    this.options = this.allSkills;
+  }
 }
