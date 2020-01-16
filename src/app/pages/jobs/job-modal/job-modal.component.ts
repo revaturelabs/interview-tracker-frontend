8e6d445245fb9f862a096897c1fbbbb085142814
Daniel Vicenti <38819937+dvicenti96@users.noreply.github.com>
2020-01-16 11:31:55 -0500

    Untested Finish
    This is not tested.

--- src/app/pages/jobs/job-modal/job-modal.component.ts
+++ src/app/pages/jobs/job-modal/job-modal.component.ts
@@ -5,6 +5,9 @@ import Skill from 'src/app/models/Skill';
 import { FormControl } from '@angular/forms';
 import { ProfileService } from 'src/app/profile.service';
 import Profile from 'src/app/models/Profile';
+import { InterviewService } from 'src/app/interview-service/interview.service';
+import Interview from 'src/app/models/Interview';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-job-modal',
@@ -14,13 +17,14 @@ import Profile from 'src/app/models/Profile';
 export class JobModalComponent implements OnInit {
 
   private modal = document.getElementById("jobModal");
+  private allInterveiws: Interview[];
   private allProfiles: Profile[];
   private allSkills: Skill[];
   private skillSelect = new FormControl();
   private candSelect = new FormControl();
   @Input() job: Job;
   
-  constructor(private skillServ: SkillService, private profServ: ProfileService) { }
+  constructor(private router: Router, private skillServ: SkillService, private profServ: ProfileService, private intServ: InterviewService) { }
 
   ngOnInit() {
     this.skillServ.retrieveAllSkills().subscribe(data => {
@@ -29,6 +33,9 @@ export class JobModalComponent implements OnInit {
     this.profServ.retrieveAllProfiles().subscribe(data => {
       //this.allProfiles = data;
     });
+    this.intServ.retrieveInterviewByJobId(this.job.jobId).subscribe(data => {
+      this.allInterveiws = data;
+    })
   }
 
   closeModal()
@@ -43,6 +50,12 @@ export class JobModalComponent implements OnInit {
     {
       this.closeModal();
     }
+  }
+
+  goToInterview(interview: Interview)
+  {
+    console.log(interview);   //FOR TESTING PLS REMOVE
+    // this.router.navigate(['/interview/'+#]);
   }
 
   submitInterviews()
