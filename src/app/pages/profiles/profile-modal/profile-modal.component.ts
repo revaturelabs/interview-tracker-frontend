ffacf7ee485348467c07568ca7dbd1d305d6eae9
David Allen Archer <David.Allen.Archer@gmail.com>
2020-01-18 15:25:23 -0500

    setup modal functionality

--- src/app/pages/profiles/profile-modal/profile-modal.component.ts
+++ src/app/pages/profiles/profile-modal/profile-modal.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { InterviewService } from 'src/app/interview-service/interview.service';
 import Profile from 'src/app/models/Profile';
 import Interview from 'src/app/models/Interview';
+import { FormControl } from '@angular/forms';
+import { SkillService } from 'src/app/skill.service';
+import Skill from 'src/app/models/Skill';
 
 @Component({
   selector: 'app-profile-modal',
@@ -10,12 +13,27 @@ import Interview from 'src/app/models/Interview';
 })
 export class ProfileModalComponent implements OnInit {
 
-  constructor(interviewService: InterviewService) { }
+  constructor(private interviewService: InterviewService, private skillService: SkillService) { }
+  
+  @Input()
+  public profile: Profile;
+  public interviews: Interview[] = [];
+  public allSkillsAvailable: Skill[];
 
-  private profile: Profile;
-  private interviews: Interview[];
+  public skillSelect = new FormControl();
 
   ngOnInit() {
+    console.log(this.profile);
+    if (this.profile) {
+      this.interviewService.retrieveInterviewsByProfileId(this.profile.id).subscribe( data => {
+        this.interviews = data;
+        console.log(this.interviews);
+      });
+    }
+    this.skillService.retrieveAllSkills().subscribe( skills => this.allSkillsAvailable = skills );
   }
 
+  close() {
+    document.getElementById(this.profile.id.toString()).style.display = 'none';
+  }
 }
