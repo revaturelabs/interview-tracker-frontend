d13975896af0e6ab9b90edf4178f501288a5d865
wonder590 <shahen.r.melkonian@gmail.com>
2020-01-20 14:55:44 -0500

    Persistence of a new job object

--- src/app/create-jobs/create-jobs.component.ts
+++ src/app/create-jobs/create-jobs.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { ProfileService } from '../profile.service';
 import { SkillService } from '../skill.service';
+import { JobServiceService } from '../Job-Service/job-service.service';
 import { FormControl } from '@angular/forms';
 import Skill from 'src/app/models/Skill';
+import Jobs from 'src/app/models/Job';
+import Job from 'src/app/models/Job';
 
 @Component({
   selector: 'app-create-jobs',
@@ -11,12 +14,31 @@ import Skill from 'src/app/models/Skill';
 })
 export class CreateJobsComponent implements OnInit {
 
-  constructor(private profServ: ProfileService, private skillServ: SkillService) { }
+  constructor(private profServ: ProfileService, private skillServ: SkillService, private jobServ: JobServiceService) { }
 
   @Output() selections = new FormControl();
+  newJob: Job;
   title = 'Skills';
   options: Skill[];
   allSkills: Skill[];
+  
+
+  createJob(){
+    this.newJob.skills = this.selections.value;
+    this.jobServ.saveJob(this.newJob).subscribe(data =>{
+      console.log(data);
+      switch(data){
+        case true:
+          break;
+        case false:
+          break;
+        default:
+          return this.newJob;
+
+      }
+    });
+    
+  }
 
   termChanged(event) {
     this.options = this.allSkills.filter(el =>
@@ -33,5 +55,6 @@ export class CreateJobsComponent implements OnInit {
       .retrieveAllSkills()
       .subscribe(list => {this.allSkills = list; console.log(list)});
     this.options = this.allSkills;
+    this.newJob = new Job(-1, "", "", null, false,[]);
   }
 }
