02dc6c9f9dbd35e022b00d97db8c2924db0fad2d
mrhunt715 <mrhunt715@gmail.com>
2020-01-31 14:33:36 -0500

    added getAllJobs

--- src/app/Job-Service/job.service.spec.ts
+++ src/app/Job-Service/job.service.spec.ts
@@ -1,14 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { JobService } from './job.service';
+import { of } from 'rxjs/internal/observable/of';
 
 describe('JobService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule]
-  }));
+  let jobService: JobService; // added for testing
+
+  beforeEach(() =>{
+    TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [JobService] 
+  });
+  jobService = TestBed.get(JobService); //added for testing
+});
 
   it('should be created', () => {
-    const service: JobService = TestBed.get(JobService);
-    expect(service).toBeTruthy();
+    // const service: JobService = TestBed.get(JobService);
+    expect(jobService).toBeTruthy();
+  });
+  describe('getAllJobs', () => {
+    it('it should return all jobs', () => {
+      const jobResponse =[
+        {id: 1,
+        title: "Developer",
+        description: "test",
+        location: "Chicago",
+        isFilled: true,
+        skills: []
+        }];
+      let response;
+      spyOn(jobService, 'getAllJobs').and.returnValue(of(jobResponse));
+
+      jobService.getAllJobs().subscribe(res => {
+        response = res;
+      });
+      expect(response).toEqual(jobResponse);
+    });
   });
 });
