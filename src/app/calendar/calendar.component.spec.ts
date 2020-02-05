37625af23b330a9f14d48f76158a55dcd6d26a13
Steffano <smalagadev@gmail.com>
2020-02-05 11:54:22 -0500

    Calendar component tests created and passed

--- src/app/calendar/calendar.component.spec.ts
+++ src/app/calendar/calendar.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalendarComponent } from './calendar.component';
-import { CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('CalendarComponent', () => {
   let component: CalendarComponent;
