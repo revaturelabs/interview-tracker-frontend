46b9fa2524ab5307521abafb8a03e6a28ff34728
Steffano <smalagadev@gmail.com>
2020-02-07 12:49:44 -0500

    should created test created and passed

--- src/app/common/mini-searchbar/mini-searchbar.component.spec.ts
+++ src/app/common/mini-searchbar/mini-searchbar.component.spec.ts
@@ -1,14 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { MiniSearchbarComponent } from './mini-searchbar.component';
 
-xdescribe('MiniSearchbarComponent', () => {
+describe('MiniSearchbarComponent', () => {
   let component: MiniSearchbarComponent;
   let fixture: ComponentFixture<MiniSearchbarComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MiniSearchbarComponent ]
+      declarations: [ MiniSearchbarComponent ],
+      imports: [ FormsModule ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
