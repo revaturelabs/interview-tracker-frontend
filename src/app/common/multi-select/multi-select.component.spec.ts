9c98dfbb5d42948bd029ca59cb29e4e67557f916
Steffano <smalagadev@gmail.com>
2020-02-07 14:45:02 -0500

    creation, sendSelections, event emmitter tests created and passed

--- src/app/common/multi-select/multi-select.component.spec.ts
+++ src/app/common/multi-select/multi-select.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { MultiSelectComponent } from './multi-select.component';
+import { DebugElement, ElementRef } from '@angular/core';
 
-xdescribe('MultiSelectComponent', () => {
+describe('MultiSelectComponent', () => {
   let component: MultiSelectComponent;
   let fixture: ComponentFixture<MultiSelectComponent>;
+  let de: DebugElement;
+  let matOption: ElementRef;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MultiSelectComponent ]
+      declarations: [ MultiSelectComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
@@ -17,9 +23,25 @@ xdescribe('MultiSelectComponent', () => {
     fixture = TestBed.createComponent(MultiSelectComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    de = fixture.debugElement;
+    matOption = de.query(By.css('mat-option'));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('sendSelections()', ()=> {
+    it('should emit when sendSelection is called', () => {
+      spyOn(component.emitSelections, 'emit');
+      component.sendSelections();
+      expect(component.emitSelections.emit).toHaveBeenCalled();
+    });
+
+    xit('should emit when click is performed', () => {
+      spyOn(component.emitSelections, 'emit');
+      matOption.nativeElement.click();
+      expect(component.emitSelections.emit).toHaveBeenCalled();
+    });
   });
 });
