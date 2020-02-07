6682f5372a5f33d5a4e79078b4012be210bc1020
acw154 <acw154@gmail.com>
2020-02-07 11:14:24 -0500

    removed useless tests

--- src/app/interview-service/interview.service.spec.ts
+++ src/app/interview-service/interview.service.spec.ts
@@ -1,10 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { InterviewService } from './interview.service';
-import { profile } from 'console';
-import Profile from '../models/Profile';
-import Job from '../models/Job';
-import { of } from 'rxjs/internal/observable/of';
 
 describe('InterviewService', () => {
 let interviewService: InterviewService;
@@ -20,24 +16,5 @@ let interviewService: InterviewService;
   it('should be created', () => {
     // const service: InterviewService = TestBed.get(InterviewService);
     expect(interviewService).toBeTruthy();
-  });
-  describe('retrieveInterviewById', () => {
-    it('it should return the interview by an Id', () => {
-      const interviewResponse =[{
-          id: 0,
-          profile: Profile,
-          date: Date,
-          isCompleted: true,
-          job: Job,
-          users: []
-      }];
-        // let response;
-        // spyOn(interviewService, 'retrieveInterviewById').and.returnValue(of());
-
-        // interviewService.retrieveInterviewById(0).subscribe(res => {
-        //   response = res;
-        // });
-        // expect(response).toEqual(interviewResponse);
-    });
   });
 });
