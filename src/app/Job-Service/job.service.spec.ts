a3fd11df1007e21d089bb70dc70297e715b64dad
acw154 <acw154@gmail.com>
2020-02-12 11:40:06 -0500

    Created job service tests

--- src/app/Job-Service/job.service.spec.ts
+++ src/app/Job-Service/job.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, getTestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { JobService } from './job.service';
+import { isEqual } from 'lodash/lang';
 
 describe('JobService', () => {
   let injector: TestBed;
@@ -22,5 +23,56 @@ describe('JobService', () => {
 afterEach(() => {
   httpMock.verify();
 });
-  
+
+  describe('getAllJobs', () => {
+    it('should throw an http error if the request is bad', async() => {
+      let response: any;
+      let errResponse: any;
+      jobService.getAllJobs().subscribe(res => response = res, err => errResponse = err);
+      let req = httpMock.expectOne('http://localhost:8765/interview-service/jobs');
+      expect(req.request.method).toBe('GET');
+      req.flush(data, mockErrorResponse);
+      expect(isEqual(errResponse, data)).toBe(true);
+      httpMock.verify();
+    });
+  });
+
+  describe('getAllJobAtPage', () => {
+    it('should throw an http error if the request is bad', async() => {
+      let response: any;
+      let errResponse: any;
+      jobService.getAllJobAtPage(3).subscribe(res => response = res, err => errResponse = err);
+      let req = httpMock.expectOne('http://localhost:8765/interview-service/jobs/3?filtertext=&skillids=');
+      expect(req.request.method).toBe('GET');
+      req.flush(data, mockErrorResponse);
+      expect(isEqual(errResponse, data)).toBe(true);
+      httpMock.verify();
+    });
+  });
+
+  describe('getByTitle', () => {
+    it('should throw an http error if the request is bad', async() => {
+      let response: any;
+      let errResponse: any;
+      jobService.getByTitle('java').subscribe(res => response = res, err => errResponse = err);
+      let req = httpMock.expectOne('http://localhost:8765/interview-service/jobs/title/java');
+      expect(req.request.method).toBe('GET');
+      req.flush(data, mockErrorResponse);
+      expect(isEqual(errResponse, data)).toBe(true);
+      httpMock.verify();
+    });
+  });
+
+  describe('getByTitlePaged', () => {
+    it('should throw an http error if the request is bad', async() => {
+      let response: any;
+      let errResponse: any;
+      jobService.getByTitlePaged('java', 2).subscribe(res => response = res, err => errResponse = err);
+      let req = httpMock.expectOne('http://localhost:8765/interview-service/jobs/title/java/2');
+      expect(req.request.method).toBe('GET');
+      req.flush(data, mockErrorResponse);
+      expect(isEqual(errResponse, data)).toBe(true);
+      httpMock.verify();
+    });
+  });
 });
